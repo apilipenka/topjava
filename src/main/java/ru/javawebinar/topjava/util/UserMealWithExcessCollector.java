e662f263b1c2ff4036acc3bd50f7ff1a5085c144
Aliaksandr Pilipenka (Ext) <ext.aliaksandr.pilipenka@tieto.com>
2024-01-29 18:55:36 +0100

    HW0 collector has been changed
    Now we use UserMealWithExcess in accumulator

--- src/main/java/ru/javawebinar/topjava/util/UserMealWithExcessCollector.java
+++ src/main/java/ru/javawebinar/topjava/util/UserMealWithExcessCollector.java
@@ -12,13 +12,13 @@ import java.util.function.BinaryOperator;
 import java.util.function.Function;
 import java.util.function.Supplier;
 import java.util.stream.Collector;
-import java.util.stream.Collectors;
 
-public class UserMealWithExcessCollector implements Collector<UserMeal, List<UserMeal>, List<UserMealWithExcess>> {
-    LocalTime startTime;
-    LocalTime endTime;
-    int caloriesPerDay;
-    Map<LocalDate, Integer> totalCaloriesByDateMap = new HashMap<>();
+public class UserMealWithExcessCollector implements Collector<UserMeal, List<UserMealWithExcess>, List<UserMealWithExcess>> {
+    private final LocalTime startTime;
+    private final LocalTime endTime;
+    private final int caloriesPerDay;
+    private final Map<LocalDate, Integer> totalCaloriesByDateMap = new HashMap<>();
+    private final Map<LocalDate, AtomicBoolean> isExcessByDateMap = new HashMap<>();
 
     public UserMealWithExcessCollector(LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
         this.startTime = startTime;
@@ -27,39 +27,41 @@ public class UserMealWithExcessCollector implements Collector<UserMeal, List<Use
     }
 
     @Override
-    public Supplier<List<UserMeal>> supplier() {
+    public Supplier<List<UserMealWithExcess>> supplier() {
         return ArrayList::new;
     }
 
     @Override
-    public BiConsumer<List<UserMeal>, UserMeal> accumulator() {
+    public BiConsumer<List<UserMealWithExcess>, UserMeal> accumulator() {
 
         return (list, userMeal) -> {
-            totalCaloriesByDateMap.put(userMeal.getDateTime().toLocalDate(),
-                    totalCaloriesByDateMap.getOrDefault(userMeal.getDateTime().toLocalDate(), 0) + userMeal.getCalories());
+            LocalDate userMealLocalDate = userMeal.getDateTime().toLocalDate();
+
+            totalCaloriesByDateMap.put(userMealLocalDate,
+                    totalCaloriesByDateMap.getOrDefault(userMealLocalDate, 0) + userMeal.getCalories());
+
+            AtomicBoolean isExcess = isExcessByDateMap.getOrDefault(userMealLocalDate,
+                    new AtomicBoolean(totalCaloriesByDateMap.get(userMealLocalDate) > caloriesPerDay));
+            isExcess.set(totalCaloriesByDateMap.get(userMealLocalDate) > caloriesPerDay);
+            isExcessByDateMap.put(userMealLocalDate, isExcess);
+
             if (TimeUtil.isBetweenHalfOpen(userMeal.getDateTime().toLocalTime(), startTime, endTime))
-                list.add(userMeal);
+                list.add(new UserMealWithExcess(userMeal.getDateTime(), userMeal.getDescription(),
+                        userMeal.getCalories(), isExcessByDateMap.get(userMeal.getDateTime().toLocalDate())));
         };
     }
 
     @Override
-    public BinaryOperator<List<UserMeal>> combiner() {
+    public BinaryOperator<List<UserMealWithExcess>> combiner() {
         return (list1, list2) -> {
-
-            list2.forEach(userMeal -> totalCaloriesByDateMap.put(userMeal.getDateTime().toLocalDate(),
-                    totalCaloriesByDateMap.getOrDefault(userMeal.getDateTime().toLocalDate(), 0) + userMeal.getCalories()));
-
             list1.addAll(list2);
             return list1;
         };
     }
 
     @Override
-    public Function<List<UserMeal>, List<UserMealWithExcess>> finisher() {
-        return list -> list.stream()
-                .map(userMeal -> new UserMealWithExcess(userMeal.getDateTime(), userMeal.getDescription(), userMeal.getCalories(),
-                        new AtomicBoolean(totalCaloriesByDateMap.get(userMeal.getDateTime().toLocalDate()) > caloriesPerDay)))
-                .collect(Collectors.toList());
+    public Function<List<UserMealWithExcess>, List<UserMealWithExcess>> finisher() {
+        return list -> list;
     }
 
     @Override
