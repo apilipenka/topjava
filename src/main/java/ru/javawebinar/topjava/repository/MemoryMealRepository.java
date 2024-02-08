b798e0c4f32ff91b5debd7a9ef01f43061a0bf3f
Aliaksandr Pilipenka (Ext) <ext.aliaksandr.pilipenka@tieto.com>
2024-02-08 19:18:57 +0100

    HW1 used datetime-local in the meal.jsp

--- src/main/java/ru/javawebinar/topjava/repository/MemoryMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/MemoryMealRepository.java
@@ -1,37 +1,44 @@
 package ru.javawebinar.topjava.repository;
 
+import org.slf4j.Logger;
 import ru.javawebinar.topjava.model.Meal;
 
 import java.time.LocalDateTime;
 import java.time.Month;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 
+import static org.slf4j.LoggerFactory.getLogger;
+
 public class MemoryMealRepository implements Repository<Meal, Integer> {
-    private static final Map<Integer, Meal> meals = new ConcurrentHashMap<>();
-    private static final AtomicInteger idCounter = new AtomicInteger(0);
+    private static final Logger log = getLogger(MemoryMealRepository.class);
+    private final Map<Integer, Meal> meals = new ConcurrentHashMap<>();
+    private final AtomicInteger idCounter = new AtomicInteger(0);
 
     public MemoryMealRepository() {
-        create(new Meal(null, LocalDateTime.of(2020, Month.JANUARY, 30, 10, 0),
-                "Завтрак", 500));
-        create(new Meal(null, LocalDateTime.of(2020, Month.JANUARY, 30, 13, 0),
-                "Обед", 1000));
-        create(new Meal(null, LocalDateTime.of(2020, Month.JANUARY, 30, 20, 0),
-                "Ужин", 500));
-        create(new Meal(null, LocalDateTime.of(2020, Month.JANUARY, 31, 0, 0),
-                "Еда на граничное значение", 100));
-        create(new Meal(null, LocalDateTime.of(2020, Month.JANUARY, 31, 10, 0),
-                "Завтрак", 1000));
-        create(new Meal(null, LocalDateTime.of(2020, Month.JANUARY, 31, 13, 0),
-                "Обед", 500));
-        create(new Meal(null, LocalDateTime.of(2020, Month.JANUARY, 31, 20, 0),
-                "Ужин", 410));
+        Arrays.asList(
+                        new Meal(null, LocalDateTime.of(2020, Month.JANUARY, 30, 10, 0),
+                                "Завтрак", 500),
+                        new Meal(null, LocalDateTime.of(2020, Month.JANUARY, 30, 13, 0),
+                                "Обед", 1000),
+                        new Meal(null, LocalDateTime.of(2020, Month.JANUARY, 30, 20, 0),
+                                "Ужин", 500),
+                        new Meal(null, LocalDateTime.of(2020, Month.JANUARY, 31, 0, 0),
+                                "Еда на граничное значение", 100),
+                        new Meal(null, LocalDateTime.of(2020, Month.JANUARY, 31, 10, 0),
+                                "Завтрак", 1000),
+                        new Meal(null, LocalDateTime.of(2020, Month.JANUARY, 31, 13, 0),
+                                "Обед", 500),
+                        new Meal(null, LocalDateTime.of(2020, Month.JANUARY, 31, 20, 0),
+                                "Ужин", 410))
+                .forEach(this::create);
     }
 
-    public static synchronized Integer createId() {
+    public synchronized Integer createId() {
         return idCounter.incrementAndGet();
     }
 
@@ -55,7 +62,7 @@ public class MemoryMealRepository implements Repository<Meal, Integer> {
 
     @Override
     public Meal update(Meal meal) {
-        return meals.replace(meal.getId(), meal);
+        return meals.computeIfPresent(meal.getId(), (i, oldmeal) -> meal);
     }
 
     @Override
